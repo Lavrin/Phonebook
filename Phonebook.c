2dd17f873d7778370fdbfad0b96fcf16029d68f3
Lavrin <Ivan4www@gmail.com>
2017-03-18 00:30:48 +0700

    Update Phonebook.c

--- Phonebook.c
+++ Phonebook.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 int i=0;
+int counter =0;
 
 struct handbook{
     int flag;
@@ -18,10 +19,9 @@ void EnteredField(char *EnterName, char *EnterNumber){
 
 int comstr(char *str1, char *str2){
     int done = 0;
-    printf("strlen1: %d - %s, strlen2: %d - %s\n", strlen(str1), str1,  strlen(str2), str2);
     if (strlen(str1)==strlen(str2)){
-	for(i=0; i<strlen(str1); i++){
-	    if(str1[i] == str2[i]){
+	for(counter=0; counter<strlen(str1); counter++){
+	    if(str1[counter] == str2[counter]){
 		done = 1;
 	    } else {
 		done = 0;
@@ -33,13 +33,14 @@ int comstr(char *str1, char *str2){
 }
 
 int main(){
-    int help = 0, counter;
+    int help = 0;
     char command[20];
     char add[] = "add";
     char list[] = "list";
     char search[] = "search";
     char delete[] = "delete";
     char quit[] = "quit";
+    char yes[] = "y";
     for(i=0; i<10; i++) phonebook[i].flag = 0;
     while(comstr(command, quit) != 1){
 	printf("Введите команду\n");
@@ -55,34 +56,54 @@ int main(){
 	    if (help==10){
 		printf("Свободных полей для записи нет. Желаете перзаписать? (y/n)\n");
 		scanf("%s", command);
-		if (command == "y"){
+		if (comstr(command, yes) == 1){
 		    printf("Какое поле хотите перезаписать? (введите имя)\n");
 		    scanf("%s", command);
-		    for(i=0; i<10 && phonebook[i].name == command; i++){
-			EnteredField(phonebook[i].name, phonebook[i].number);
-			break;
+		    for(i=0; i<10; i++){
+			if(phonebook[i].name == command){
+			    EnteredField(phonebook[i].name, phonebook[i].number);
+			    break;
+			}
 		    }
 		} else break;
 	    }
 	}
 	if(comstr(command, list) == 1){
-	    for(counter = 10, i=0; i<10 && phonebook[i].flag==1; i++, counter--){
-		printf("Имя: %s, Номер: %s %d\n", phonebook[i].name, phonebook[i].number, i);
+	    for(counter = 10, i=0; i<10; i++){
+		if(phonebook[i].flag==1){
+		    printf("Имя: %s, Номер: %s\n", phonebook[i].name, phonebook[i].number);
+		    counter--;
+		}
 	    }
 	    if(counter>0) printf("Осталось %d свободных полей\n", counter);
-	    if(counter == 0) printf("свободных полей не осталось\n");
+	    if(counter <= 0) printf("Свободных полей не осталось\n");
 	}
 	if(comstr(command, search) ==1){
 	    printf("Введите имя искомого контакта\n");
 	    scanf("%s", command);
-	    for(i=0; i<10; i++){
-		printf("name: %s, command: %s, comstr: %d\n", phonebook[i].name, command, comstr(command, phonebook[i].name));
-		if(comstr(phonebook[i].name, command) == 1){
+	    for(i=0, help=0; i<10; i++){
+		if(comstr(phonebook[i].name, command) == 1 && phonebook[i].flag == 1){
 		    printf("Номер телефона: %s\n", phonebook[i].number);
+		    help = 1;
 		}
 	    }
+	    if(help==0){
+		printf("Такого контакта нет\n");
+	    }
 	}
-	for(i=0; i<10; i++) printf("name: %s; number: %s\n", phonebook[i].name, phonebook[i].number);
+	if(comstr(command, delete) == 1){
+	    printf("Какой контакт хотите удалить? (Введите имя)\n");
+	    scanf("%s", command);
+	    for(i=0, help=0; i<10; i++){
+		if(comstr(command, phonebook[i].name) == 1){
+		    phonebook[i].flag = 0;
+		    help = 1;
+		}
+	    }
+	    if(help==0){
+		printf("Такого контакта нет\n");
+	    }
+	}
     }
     return 0;
 }
